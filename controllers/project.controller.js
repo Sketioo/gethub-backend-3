74dc3be4fd8934dab012d373d575ddc108678273
AmurTigro <martiohusein27@gmail.com>
2024-06-02 23:05:57 +0700

    "Added getAllTask function to project controller and corresponding route to retrieve all tasks for a project"

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -148,6 +148,37 @@ const postProject = async (req, res) => {
   }
 };
 
+const getAllTask = async (req, res) => {
+  try {
+    const tasks = await models.Project_Task.findAll({
+      where: { project_id: req.params.id }
+    });
+
+    if (!tasks || tasks.length === 0) {
+      return res.status(404).json({
+        success: false,
+        data: [],
+        message: 'Tidak ada tugas yang ditemukan',
+        error_code: 404
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: tasks,
+      message: 'Semua tugas berhasil diambil',
+      error_code: 0
+    })
+  } catch (error) {
+    console.log('error: ', error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+      error_code: 500
+    })
+  }
+}
+
 const postTask = async (req, res) => {
   const { task_number, task_description, task_status, task_feedback } = req.body;
   const { id } = req.params;
@@ -1003,6 +1034,7 @@ module.exports = {
   getAllProjectsAdmin,
   updateProjectActiveStatus,
   postTask,
+  getAllTask,
   getProjectList,
   getProjectBidders,
   getUserJobStatsAndBids,
